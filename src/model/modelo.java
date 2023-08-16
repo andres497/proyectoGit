6775e2fcb2a11bd057d96275c0f84e06692e83e5
User <User@DESKTOP-5KNJAMH>
2023-08-16 16:24:36 -0500

    Carga inicial

---
+++ src/model/modelo.java
@@ -0,0 +1,28 @@
+package model;
+
+public class modelo {
+
+    private int valor1 = 28;
+    private int valor2 = 69;
+    private int total = sumar();
+
+    public modelo() {
+    }
+
+    public void setValor1(int valor1) {
+        this.valor1 = valor1;
+    }
+
+    public int getValor1() {
+        return valor1;
+    }
+
+    public void setValor2(int valor2) {
+        this.valor2 = valor2;
+    }
+
+    private int sumar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
